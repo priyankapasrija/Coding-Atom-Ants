d50a0b2a0dc00e1f65e3a06be3277ad3c5406795
Lukileila <142001977+Lukileila@users.noreply.github.com>
2023-09-17 13:46:06 +0200

    intermediate steps

--- app.js
+++ app.js
@@ -4,19 +4,33 @@ const btn     = document.querySelector(".add");
 const result  = document.querySelector(".result");
 const delBtn  = document.querySelector(".delete");
 
+const taskcreator =(tasktext)=>{
+  let li = document.createElement("li");
+  console.log(li);
+  console.log("asdfasdfasdf");
+  li.innerHTML = input.value;
+  result.appendChild(li);
+  input.value = "";
+
+  let delBtn = document.createElement("span");
+  delBtn.innerHTML = "Delete";
+  li.appendChild(delBtn);
+
+  let first = document.createElement("div");
+  li.appendChild(first);
+  let middle = document.createElement("p");
+  li.appendChild(middle);
+  let last = document.createElement("div");
+  li.appendChild(last);
+};
+
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   let inputField = input.value.trim();
   if (inputField === "") {
     alert("please add your task");
   } else {
-    let li = document.createElement("li");
-    li.innerHTML = input.value;
-    result.appendChild(li);
-    input.value = "";
-    let delBtn = document.createElement("span");
-    delBtn.innerHTML = "Delete";
-    li.appendChild(delBtn);
+    taskcreator ();
   }
 });
 
